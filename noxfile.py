75a1906a72836269bc8a292f7c2613e6ff8e67b7
bradflaugher <16511019+bradflaugher@users.noreply.github.com>
2025-09-20 08:56:58 -0400

    Improve BSL implementation with headers and clearer licensing
    - Add BSL headers to all Python source files with copyright notice, license reference, Change Date, and Change License
    - Update LICENSE file with specific Change Date (September 20, 2027) for version 1.x
    - Add license metadata to pyproject.toml for better package discoverability
    - Ensure compliance with BSL 1.1 best practices as outlined in MariaDB adoption guide

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,17 @@
-"""Automation for test and lint tasks."""
+# Copyright (c) 2025 ElcanoTek
+#
+# This file is part of Victoria Terminal.
+#
+# This software is licensed under the Business Source License 1.1.
+# You may not use this file except in compliance with the license.
+# You may obtain a copy of the license at
+#
+#     https://github.com/ElcanoTek/victoria-terminal/blob/main/LICENSE
+#
+# Change Date: 2027-09-20
+# Change License: GNU General Public License v3.0 or later
 
+"""Automation for test and lint tasks."""
 from __future__ import annotations
 
 from pathlib import Path
